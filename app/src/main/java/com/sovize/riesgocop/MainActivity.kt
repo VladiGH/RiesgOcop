dd5933f1d72ea48b9a13e62234afbfeeefcf7ca1
Unknown <shulkmaster@gmail.com>
2019-05-06 23:41:57 -0600

    Firts sing in with Google implementations

--- app/src/main/java/com/sovize/riesgocop/MainActivity.kt
+++ app/src/main/java/com/sovize/riesgocop/MainActivity.kt
@@ -1,20 +1,69 @@
 package com.sovize.riesgocop
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import com.sovize.riesgocop.activities.ReportActivity
+import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.tasks.Task
+import android.content.Intent
+import com.google.android.gms.common.api.ApiException
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val signInCode = 2
+    private val tag = "MainActivity"
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.btn_login)?.setOnClickListener {
-            startActivity(
-                Intent(this@MainActivity, ReportActivity::class.java)
-            )
+        mGoogleSignInClient = GoogleSignIn.getClient(
+            this, GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+        )
+        findViewById<SignInButton>(R.id.btn_login)?.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, signInCode)
         }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account == null) {
+            findViewById<TextView>(R.id.textView).text = "Usuario nullo"
+        } else {
+            findViewById<TextView>(R.id.textView).text = account.displayName
+        }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        
+        if (requestCode == signInCode) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(task: Task<GoogleSignInAccount>?) {
+        try {
+            val account = task?.getResult(ApiException::class.java)
+            findViewById<TextView>(R.id.textView).text = account?.displayName?:"Null user"
+
+        } catch (e: ApiException) {
+            findViewById<TextView>(R.id.textView).text = "signInResult: failed code = ${e.statusCode}"
+        }
+
     }
 }

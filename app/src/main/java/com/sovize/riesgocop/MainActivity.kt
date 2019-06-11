61eca89418ea86cd74e0ce18451f5ddbc5ce89fe
Andre <00103017@uca.edu.sv>
2019-06-11 12:18:37 -0600

    adding navigation view with the menu

--- app/src/main/java/com/sovize/riesgocop/MainActivity.kt
+++ app/src/main/java/com/sovize/riesgocop/MainActivity.kt
@@ -3,9 +3,12 @@ package com.sovize.riesgocop
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import android.view.MenuItem
 import android.widget.ImageView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -20,6 +23,7 @@ import com.sovize.riesgocop.views.activities.ProfileActivity
 import com.sovize.riesgocop.views.activities.ReportActivity
 import com.sovize.riesgocop.views.fragments.IssuesList
 import com.sovize.riesgocop.views.fragments.QuickBar
+import kotlinx.android.synthetic.main.main_activity.*
 
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -53,8 +57,18 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             startActivity(Intent(this@MainActivity, ProfileActivity::class.java))
         }
         vmMain.getUserData().observe(this, userObserver)
-    }
 
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, findViewById(R.id.mainBar), R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+
+        drawer_layout.addDrawerListener(toggle)
+
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+/*
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             0 -> {
@@ -65,6 +79,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         Log.i(AppLogger.mainActivity, item.itemId.toString())
         return true
     }
+*/
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -89,6 +104,43 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onResume() {
         super.onResume()
         vmMain.setUserState()
+    }
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+
+    // TODO (14.2) Funcion que recibe el ID del elemento tocado
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_logOut -> {
+
+            }
+        }
+
+        // TODO (15) Cuando se da click a un opcion del menu se cierra de manera automatica
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 
 }

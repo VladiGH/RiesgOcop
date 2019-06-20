6534b48b1826378da6510330084e891ae65621f1
fernandom721 <00076617@uca.edu.sv>
2019-06-20 12:33:34 -0600

    Calculator Adapter

---
+++ app/src/main/java/com/sovize/riesgocop/models/CalculatorProduct.kt
@@ -0,0 +1,8 @@
+package com.sovize.riesgocop.models
+
+data class CalculatorProduct(
+    val nombre: String ="N/A",
+    val cantidad: Int = 0,
+    val valorunidad: Int= 0,
+    val total: Int=0
+)
---
+++ app/src/main/java/com/sovize/riesgocop/views/activities/Calculator.kt
@@ -0,0 +1,13 @@
+package com.sovize.riesgocop.views.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Calculator: AppCompatActivity(){
+
+    val Total: Int?= 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

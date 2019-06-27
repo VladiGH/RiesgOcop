be64f4c1553a7e33a9f1a58518143d7a71d763be
shulkmaster <00088116@uca.edu.sv>
2019-06-26 22:16:19 -0600

    priimer fragmento de solo vistas

--- app/src/main/java/com/sovize/ultracop/views/fragments/ReportDetailFragment.kt
+++ app/src/main/java/com/sovize/ultracop/views/fragments/ReportDetailFragment.kt
@@ -6,50 +6,36 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.sovize.ultracop.R
-import com.sovize.ultracop.models.Report
-import java.text.SimpleDateFormat
-import java.util.*
+import com.sovize.ultracop.models.AccidentReport
 
-class ReportDetailFragment: Fragment() {
+class ReportDetailFragment : Fragment() {
 
-    var report: Report? = Report()
-    var fecha = Date()
-    var formatFecha = SimpleDateFormat("dd-MM-yy")
+    var report = AccidentReport()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        val view = inflater.inflate(R.layout.viewer_report, container, false)
-        bindData(view)
-        return view
+        val v = inflater.inflate(R.layout.viewer_report, container, false)
 
-    }
+        val name = "${resources.getString(R.string.person_injured_name)}: ${report.personInjuredName}"
+        val gender = "${resources.getString(R.string.gender_of_the_person_injured)}: ${report.personInjuredGender}"
+        val place = "${resources.getString(R.string.place_of_attention)}: ${report.placeOfAttention}"
+        val ambulance = "${resources.getString(R.string.was_necessary_an_ambulance)}: ${report.ambullance}"
+        val date = "${resources.getString(R.string.date)}: ${report.date}"
+        val severity = "${resources.getString(R.string.severity)}: ${report.severityLevel}"
 
-    fun bindData(view: View){
-        view.findViewById<CollapsingToolbarLayout>(R.id.collapsingtoolbarviewer_reportname).title = report?.title
-        view.findViewById<TextView>(R.id.app_bar_rating_danger_viewer).text = report?.danger.toString()
-        view.findViewById<TextView>(R.id.location).text = report?.location
-        view.findViewById<TextView>(R.id.description_report_viewer).text = report?.description
-        view.findViewById<TextView>(R.id.tv_date).text = formatFecha.format(fecha).toString()
-    }
-
-    fun setData(report: Report){
-        view?.findViewById<CollapsingToolbarLayout>(R.id.collapsingtoolbarviewer_reportname)?.title = report.title
-        view?.findViewById<TextView>(R.id.app_bar_rating_danger_viewer)?.text = report.danger.toString()
-        view?.findViewById<TextView>(R.id.location)?.text = report.location
-        view?.findViewById<TextView>(R.id.description_report_viewer)?.text = report.description
-        view?.findViewById<TextView>(R.id.tv_date)?.text = formatFecha.format(fecha).toString()
-    }
-
-    companion object {
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(newReport: Report?):ReportDetailFragment{
-            val newFragment= ReportDetailFragment()
-            newFragment.report = newReport
-            return newFragment
-
+        v?.let {
+            it.findViewById<TextView>(R.id.nameOfPerson).text = name
+            it.findViewById<TextView>(R.id.genderOfPerson).text = gender
+            it.findViewById<TextView>(R.id.description_report_viewer).text = resources.getString(R.string.description)
+            it.findViewById<TextView>(R.id.description_content).text = report.description
+            it.findViewById<TextView>(R.id.placeAttention).text = place
+            it.findViewById<TextView>(R.id.ambulance).text = ambulance
+            it.findViewById<TextView>(R.id.tv_date).text = date
+            it.findViewById<TextView>(R.id.severity).text = severity
         }
+        return v
     }
+
+
 }
